7696b21ff8af5b3116514d2bb8de6455904ec8ba
jlopezsi <jordi.lopez@uab.es>
2016-04-01 11:12:05 +0200

    positioning-pda

--- pda.pos/pda_positioning.R
+++ pda.pos/pda_positioning.R
@@ -1,24 +1,20 @@
-
-#lectura de datos g20per.txt con análisis interno
 library(MASS)
 #this code install packages if they aren't already installed
-if(!require(ggplot2)){
-  install.packages("ggplot2")
+if(!require(c("ggplot2", "XLConnect")){
+  install.packages("ggplot2", "XLConnect")
   library(ggplot)#load the packege
 }
-
 source("marketing-models.R")
-# -- Read in Average Perceptions table -- #
-#require(XLConnect)
+require(XLConnect)
 #pda <- loadWorkbook(file.choose())
-#pda_per <- readWorksheet(pda, rownames=1, sheet = "perceptions", header = TRUE)
+#perceptions <- readWorksheet(pda, rownames=1, sheet = "perceptions", header = TRUE)
 #?readWorksheet
-pda_per <- read.table("pda_per.txt", sep = "\t", dec = "," , row.names=1, header=T)
-head(pda_per)
+perceptions <- read.table("pda_per.txt", sep = "\t", dec = "," , row.names=1, header=T)
+head(perceptions)
 
-perceptions = t(pda_per) #transposing to ease analysis
+mydata = t(perceptions) #transposing to ease analysis
 
-perceptions #view the table read
+mydata #view the table read
 
 # -- Read in preferences table -- #
 #pda.pref.txt
@@ -30,18 +26,15 @@ dim(preferences) #check table dimensions
 
 preferences[1:10,] #view first 10 rows
 
-########plot perceptions
 #Compute principal components analysis with prcomp (princomp doesn't alow to perform pca with less rows than colomnes)
-perceptions.pca<-prcomp(perceptions, cor=T)
-#Check the findings, how pc are produced from original indicators
-perceptions.pca
-#Check the variation explained by principal components
-summary(perceptions.pca)
+perceptions.pca<-prcomp(mydata, cor=T)
+summary(perceptions.pca)#Check the variation explained by principal components
+perceptions.pca #Check the findings, how pc are produced from original indicators
 #Check all objects available in g20t.pca objects
 names(perceptions.pca)
 perceptions.puntos<-predict(perceptions.pca)
 #Interpret the meaning of the principal components
-cor(perceptions, perceptions.puntos)
+cor(mydata, perceptions.puntos)
 #plot pc using biplot function
 biplot(perceptions.pca, pc.biplot=T)
 ?biplot #seach for help
@@ -50,26 +43,24 @@ abline(h=0); abline(v=0) # draw horiz and vertical axes
 
 ########add preferences from a vector model##########
 #we use the correlation between the preferences data frame and the pc plotted
-preferences.puntos<-cor(t(preferences), perceptions.puntos)
+pref.puntos<-cor(t(preferences), perceptions.puntos)
 #add preferences as arrows to the biplot
-arrows(0, 0, preferences.puntos[,1], preferences.puntos[,2]) 
+arrows(0, 0, pref.puntos[,1], pref.puntos[,2]) 
 #add the indentity of consumers
-text (preferences.puntos[,1:2], labels = row.names(preferences))
+text (pref.puntos[,1:2], labels = row.names(preferences))
 
 #####repeat the process again for pc1 and pc3
 biplot(perceptions.pca, choice=c(1,3))
-preferences.puntos<-cor(t(preferences), perceptions.puntos)
-arrows(0, 0, preferences.puntos[,1], preferences.puntos[,3]) 
-text (preferences.puntos[,1:3], labels = row.names(preferences))
+arrows(0, 0, pref.puntos[,1], pref.puntos[,3]) 
+text (pref.puntos[,1:3], labels = row.names(preferences))
 
 #And for compontens 2 and 3
 biplot(perceptions.pca, choice=c(2,3))
-preferences.puntos<-cor(t(preferences), perceptions.puntos)
-arrows(0, 0, preferences.puntos[,2], preferences.puntos[,3]) 
-text (preferences.puntos[,2:3], labels = row.names(preferences))
+arrows(0, 0, pref.puntos[,2], pref.puntos[,3]) 
+text (pref.puntos[,2:3], labels = row.names(preferences))
 
 ####Segment preferences and plot arrows' colours  according to the classification
-pref.hclust <- hclust(dist(preferences), method="ward")
+pref.hclust <- hclust(dist(pref), method="ward")
 plot(pref.hclust)
 source("marketing-models.R")
 #centros-hclust, calcula las medias en los segmentos obtenidos con hclust
@@ -87,11 +78,10 @@ abline(h=0); abline(v=0) # draw horiz and vertical axes
 
 ########add preferences from a vector model##########
 #we use the correlation between the preferences data frame and the pc plotted
-preferences.puntos<-cor(t(preferences), perceptions.puntos)
 #add preferences as arrows to the biplot
-arrows(0, 0, preferences.puntos[,1], preferences.puntos[,2]) 
+arrows(0, 0, pref.puntos[,1], pref.puntos[,2]) 
 #add the indentity of consumers
-text (preferences.puntos[,1:2], labels = row.names(preferences))
+text (pref.puntos[,1:2], labels = row.names(preferences))
 
 ########add preferences from a verctor model##########
 biplot(perceptions.pca, pc.biplot=T)
@@ -99,9 +89,9 @@ biplot(perceptions.pca, pc.biplot=T)
 names(perceptions.pca)
 abline(h=0); abline(v=0)
 #pref.puntos<-cor(t(pref), g20t.puntos)
-arrows(0, 0, preferences.puntos[,1], preferences.puntos[,2], col=pref.kmeans3$cluster) 
+arrows(0, 0, pref.puntos[,1], pref.puntos[,2], col=pref.kmeans3$cluster) 
 #points(pref.puntos[,1:2]) #mirar de graficar en color el segmento
-text (preferences.puntos[,1:2], labels = row.names(preferences))
+text (pref.puntos[,1:2], labels = row.names(preferences))
 
 #We can plot the clusters' means instead of the individual preferences
 pref.segments <- pref.kmeans3$centers
@@ -109,11 +99,11 @@ pref.segments
 biplot(perceptions.pca, pc.biplot=T)
 # draw horitzontal and vertical lines
 abline(h=0); abline(v=0) # draw horiz and vertical axes 
+
 ########add preferences from a vector model##########
 #we use the correlation between the preferences data frame and the pc plotted
-
-pref.segments.puntos<-cor(t(pref.segments), perceptions.puntos)
 head(pref.segments.puntos)
+pref.segments.puntos<-cor(t(pref.segments), perceptions.puntos)
 #add preferences as arrows to the biplot
 arrows(0, 0, pref.segments.puntos[,1], pref.segments.puntos[,2], col='green') 
 #add the indentity of consumers
@@ -127,26 +117,25 @@ text (pref.segments.puntos[,1:2], labels = row.names(pref.segments))
 library(devtools)
 install_github("vqv/ggbiplot")
 library(ggbiplot)
-###ggplot2 needs a data.frame as argument
-preferences.puntos<-as.data.frame(preferences.puntos)
-names(preferences.puntos)
+pref.puntos<-as.data.frame(pref.puntos)
+names(pref.puntos)
 ggscreeplot(perceptions.pca)
-pda.ggbiplot<-ggbiplot(perceptions.pca,  labels=rownames(perceptions),choice=c(1,2))
-pda.ggbiplot + geom_segment(data = preferences.puntos,
+pda.ggbiplot<-ggbiplot(perceptions.pca,  labels=rownames(mydata),choice=c(1,2))
+pda.ggbiplot + geom_segment(data = pref.puntos,
                             aes(x = 0, y = 0, xend = PC1, yend = PC2 ),
                             arrow = arrow(length = unit(1/2, 'picas')), 
                             color = muted('green')) + 
-  geom_text(data=preferences.puntos, aes(PC1, PC2, label = row.names(preferences)), size=4)
+  geom_text(data=pref.puntos, aes(PC1, PC2, label = row.names(preferences)), size=4)
 
 ###Add the results of clustering preferences
 
-pda.ggbiplot<-ggbiplot(perceptions.pca,  labels=rownames(perceptions),choice=c(1,2))
-pda.ggbiplot + geom_segment(data = preferences.puntos,
+pda.ggbiplot<-ggbiplot(perceptions.pca,  labels=rownames(mydata),choice=c(1,2))
+pda.ggbiplot + geom_segment(data = pref.puntos,
                             aes(x = 0, y = 0, xend = PC1, yend = PC2),
                             arrow = arrow(length = unit(1/2, 'picas')), 
                             color = pref.kmeans3$cluster) + 
-  geom_text(data=preferences.puntos, aes(PC1, PC2, label = row.names(preferences)), size=4)
+  geom_text(data=pref.puntos, aes(PC1, PC2, label = row.names(preferences)), size=4)
 
-
+###Add the segments summary instead of individual preferences
 
 
